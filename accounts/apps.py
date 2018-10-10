b895ff517d5488c64a396f06ba900bf151ce8716
anujrathor <41207048+anujrathor@users.noreply.github.com>
2018-10-10 17:01:27 +0530

    post_issue solved

--- accounts/apps.py
+++ accounts/apps.py
@@ -5,4 +5,7 @@ class AccountsConfig(AppConfig):
     name = 'accounts'
 
     def ready(self):
-        import accounts.signals
+        from django.db.models.signals import post_save
+        from accounts.signals import create_user_profile
+        from django.contrib.auth.models import User
+        post_save.connect(create_user_profile, sender=User, dispatch_uid='create_user_profile')
--- accounts/signals.py
+++ accounts/signals.py
@@ -1,11 +1,8 @@
-from django.db.models.signals import post_save
-from django.contrib.auth.models import User
 from accounts.models import Profile
-from django.dispatch import receiver
 
-@receiver(post_save,sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
-    Profile.objects.create(user=instance)
-    Profile.save()
+    if created:
+        Profile.objects.create(user=instance)
+
 
 
